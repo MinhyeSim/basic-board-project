9a0271adc4aa6294aef73fa64793c89a88811198
MinhyeSim <smine0032@gmail.com>
2022-09-15 23:16:31 +0900

    mariaDB 연결

---
+++ src/test/java/com/basic/bord/BasicBordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basic.bord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicBordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
