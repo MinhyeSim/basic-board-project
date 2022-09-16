b54f140259c361f89a16ee841be49ecceec597c6
MinhyeSim <smine0032@gmail.com>
2022-09-16 21:36:25 +0900

    User,Post,Comment DTO

--- src/main/java/com/basic/board/User/domains/UserDAO.java
+++ src/main/java/com/basic/board/User/domains/UserDAO.java
@@ -5,6 +5,7 @@ import lombok.*;
 import org.springframework.data.repository.cdi.Eager;
 
 import javax.persistence.*;
+import javax.validation.constraints.Pattern;
 import javax.xml.soap.Text;
 
 
@@ -22,15 +23,18 @@ public class UserDAO {
     private long userId;
 
     @Column(length = 30, unique = true)
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{4,20}$", message = "아이디는 특수문자를 제외한 4~20자리여야 합니다.")
     private @NotNull String username; //user 아이디
 
     @Column(unique = true)
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private @NotNull String nickname;
 
     @Column(length = 100)
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,10}$", message = "닉네임은 특수문자를 제외한 2~10자리여야 합니다.")
     private @NotNull String password;
 
     @Column(length = 50, unique = true)
+    @Pattern(regexp = "^(?:\\w+\\.?)*\\w+@(?:\\w+\\.)+\\w+$", message = "이메일 형식이 올바르지 않습니다.")
     private @NotNull String email;
-
 }
